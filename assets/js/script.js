43ab41c588256adba39dd1df5f0f55d6a5caead8
Markus Hefner <m5zrlnlt@students.codeinstitute.net>
2024-09-20 16:00:32 +0000

    Add javascript for check/uncheck all bitters/sweet components/sour components

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,3 +41,69 @@ function checkAllLiqueurs() {
         }
     }
 }
+
+let checkboxAllBitters = document.querySelector("input[id=check-all-bitters]");
+checkboxAllBitters.addEventListener('change', checkAllBitters);
+
+/** This function selects/unselect all bitters */
+function checkAllBitters() {
+    if (this.checked === true) {
+        console.log("Checkbox is checked.."); // Checkpoint
+        let checkbox = document.getElementsByClassName('bitters');
+        for (let i = 0; i < checkbox.length; i++) {
+            console.log("yeah2"); // Checkpoint
+            checkbox[i].checked = true;
+        }
+    } else if (this.checked === false) {
+        console.log("Checkbox is not checked.."); // Checkpoint
+        let checkbox = document.getElementsByClassName('bitters');
+        for (let i = 0; i < checkbox.length; i++) {
+            console.log("yeah3"); // Checkpoint
+            checkbox[i].checked = false;
+        }
+    }
+}
+
+let checkboxAllSweets = document.querySelector("input[id=check-all-sweets]");
+checkboxAllSweets.addEventListener('change', checkAllSweets);
+
+/** This function selects/unselect all sweets */
+function checkAllSweets() {
+    if (this.checked === true) {
+        console.log("Checkbox is checked.."); // Checkpoint
+        let checkbox = document.getElementsByClassName('sweets');
+        for (let i = 0; i < checkbox.length; i++) {
+            console.log("yeah2"); // Checkpoint
+            checkbox[i].checked = true;
+        }
+    } else if (this.checked === false) {
+        console.log("Checkbox is not checked.."); // Checkpoint
+        let checkbox = document.getElementsByClassName('sweets');
+        for (let i = 0; i < checkbox.length; i++) {
+            console.log("yeah3"); // Checkpoint
+            checkbox[i].checked = false;
+        }
+    }
+}
+
+let checkboxAllSours = document.querySelector("input[id=check-all-sours]");
+checkboxAllSours.addEventListener('change', checkAllSours);
+
+/** This function selects/unselect all sours */
+function checkAllSours() {
+    if (this.checked === true) {
+        console.log("Checkbox is checked.."); // Checkpoint
+        let checkbox = document.getElementsByClassName('sours');
+        for (let i = 0; i < checkbox.length; i++) {
+            console.log("yeah2"); // Checkpoint
+            checkbox[i].checked = true;
+        }
+    } else if (this.checked === false) {
+        console.log("Checkbox is not checked.."); // Checkpoint
+        let checkbox = document.getElementsByClassName('sours');
+        for (let i = 0; i < checkbox.length; i++) {
+            console.log("yeah3"); // Checkpoint
+            checkbox[i].checked = false;
+        }
+    }
+}
