68126d151ff935c0c7dbbdaedbf22d6410a38f12
Markus Hefner <m5zrlnlt@students.codeinstitute.net>
2024-09-20 15:52:49 +0000

    Add check/uncheck all for liqueurs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,43 @@
 let checkboxAllSpirits = document.querySelector("input[id=check-all-spirits]");
-
 checkboxAllSpirits.addEventListener('change', checkAllSpirits);
 
+/** This function selects/unselect all spirits */
 function checkAllSpirits() {
-            if (this.checked === true) {
-                console.log("Checkbox is checked..");
-                let checkbox = document.getElementsByClassName('spirits');
-                for (let i = 0; i < checkbox.length; i++) {
-                    console.log("yeah2"); // Checkpoint
-                    checkbox[i].checked = true;
-                }
-                } else if (this.checked === false) {
-                    console.log("Checkbox is not checked..");
-                    let checkbox = document.getElementsByClassName('spirits');
-                for (let i = 0; i < checkbox.length; i++) {
-                    console.log("yeah3"); // Checkpoint
-                    checkbox[i].checked = false;
-                }
-                }
-            }
+    if (this.checked === true) {
+        console.log("Checkbox is checked.."); // Checkpoint
+        let checkbox = document.getElementsByClassName('spirits');
+        for (let i = 0; i < checkbox.length; i++) {
+            console.log("yeah2"); // Checkpoint
+            checkbox[i].checked = true;
+        }
+    } else if (this.checked === false) {
+        console.log("Checkbox is not checked.."); // Checkpoint
+        let checkbox = document.getElementsByClassName('spirits');
+        for (let i = 0; i < checkbox.length; i++) {
+            console.log("yeah3"); // Checkpoint
+            checkbox[i].checked = false;
+        }
+    }
+}
+
+let checkboxAllLiqueurs = document.querySelector("input[id=check-all-liqueurs]");
+checkboxAllLiqueurs.addEventListener('change', checkAllLiqueurs);
+
+/** This function selects/unselect all liqueurs */
+function checkAllLiqueurs() {
+    if (this.checked === true) {
+        console.log("Checkbox is checked.."); // Checkpoint
+        let checkbox = document.getElementsByClassName('liqueurs');
+        for (let i = 0; i < checkbox.length; i++) {
+            console.log("yeah2"); // Checkpoint
+            checkbox[i].checked = true;
+        }
+    } else if (this.checked === false) {
+        console.log("Checkbox is not checked.."); // Checkpoint
+        let checkbox = document.getElementsByClassName('liqueurs');
+        for (let i = 0; i < checkbox.length; i++) {
+            console.log("yeah3"); // Checkpoint
+            checkbox[i].checked = false;
+        }
+    }
+}
